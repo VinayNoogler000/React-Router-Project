d82f48b1a996ff271ecf36b8a28bb5bf5270c36c
VinayNoogler000 <vinaytambey000@gmail.com>
2025-11-03 21:28:43 +0530

    Add index file to export Header, Home, and Footer components

---
+++ components/index.jsx
@@ -0,0 +1,5 @@
+import Header from "../components/Header/Header.jsx";
+import Home from "../components/Home/Home.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+
+export {Header, Home, Footer};
