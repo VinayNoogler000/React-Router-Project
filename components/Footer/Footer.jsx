455003a8f1e683b777371bf3bb6a64aa4cfc2469
VinayNoogler000 <vinaytambey000@gmail.com>
2025-11-03 21:22:37 +0530

    Update Links

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
                             />
                         </Link>
                     </div>
-                    
+
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
@@ -36,7 +36,7 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/vinaynoogler000/react-router-project"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
@@ -71,9 +71,9 @@ export default function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                        © 2025
+                        <a href="https://www.vinatambey.bio/" className="hover:underline">
+                            vinaytambey
                         </a>
                         . All Rights Reserved.
                     </span>
@@ -106,7 +106,7 @@ export default function Footer() {
                             </svg>
                             <span className="sr-only">Discord community</span>
                         </Link>
-                        <Link to="#" className="text-gray-500">
+                        <Link to="https://x.com/vinaynoogler000" className="text-gray-500">
                             <svg
                                 className="w-4 h-4"
                                 aria-hidden="true"
@@ -122,7 +122,7 @@ export default function Footer() {
                             </svg>
                             <span className="sr-only">Twitter page</span>
                         </Link>
-                        <Link to="#" className="text-gray-500">
+                        <Link to="http://github.com/vinayNoogler000/" className="text-gray-500">
                             <svg
                                 className="w-4 h-4"
                                 aria-hidden="true"
