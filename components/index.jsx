61f1230f3bf47c2a807c65817d115cbec839396f
VinayNoogler000 <vinaytambey000@gmail.com>
2025-11-03 22:22:44 +0530

    Create basic GitHub page component

--- components/index.jsx
+++ components/index.jsx
@@ -2,6 +2,7 @@ import Header from "./Header/Header.jsx";
 import Home from "./Home/Home.jsx";
 import About from "./About/About.jsx";
 import Contact from "./Contact/Contact.jsx";
+import GitHub from "./GitHub/GitHub.jsx";
 import Footer from "./Footer/Footer.jsx";
 
-export {Header, Home, About, Contact, Footer};
+export {Header, Home, About, Contact, GitHub, Footer};
