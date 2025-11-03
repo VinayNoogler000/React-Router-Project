78395514e28f11464e0818ff3c744dbbb9e31e00
VinayNoogler000 <vinaytambey000@gmail.com>
2025-11-03 22:30:22 +0530

    Use  for optimizied, modern and clean client-side routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,32 +2,61 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import {Home, About, Contact} from "../components/index.jsx";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import {Home, About, Contact, GitHub} from "../components/index";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '',
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
-      },
-      {
-        path: "contact",
-        element: <Contact />
-      },
-    ]
-  }
-]);
+// 1st Way of Creating Routes (Client-Side) - Complex Version:
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       },
+//       {
+//         path: "github",
+//         element: <GitHub />
+//       },
+//     ]
+//   }
+// ]);
+
+// 2nd Way of Creating Routes (Client-Side) - Easier Version:
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<App/>}>
+//        <Route path="" element={<Home/>}></Route>
+//        <Route path="about" element={<About/>}></Route>
+//        <Route path="contact" element={<Contact/>}></Route>
+//        <Route path="github" element={<GitHub/>}></Route>
+//     </Route>
+//   )
+// );
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    {/* <RouterProvider router={router}/> */}
+
+    {/* 3rd Way of Creating Routes (Client-Side) - Easiest Way */}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<Home/>} />
+          <Route path="about" element={<About/>} />
+          <Route path="contact" element={<Contact/>} />
+          <Route path="github" element={<GitHub/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
