669da8f7d7d7e0eeadb2c5cea6cd6dd128776bb3
VinayNoogler000 <vinaytambey000@gmail.com>
2025-11-03 21:15:36 +0530

    Refractor - Formatted

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
                     <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <NavLink to="/" className={ ({isActive}) =>
+                                <NavLink to="/" className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                 }>
                                     Home
@@ -37,26 +37,19 @@ export default function Header() {
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink
-                                to="/contact"
-                                    className={({isActive}) =>
+                                <NavLink to="/contact" className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
+                                }>
                                     Contact
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink
-                                to="/github"
-                                    className={({isActive}) =>
+                                <NavLink to="/github" className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
+                                }>
                                     Github
                                 </NavLink>
                             </li>
-                            
                         </ul>
                     </div>
                 </div>
